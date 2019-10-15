ff6b5d30a37c234e5936b8a3a92bb768aaa8075c
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-15 18:14:14 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -72,7 +72,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  Movie = Movie.find
+  Movie = Movie.find_by(title: "Wat?")
+  
   __
 end
 
