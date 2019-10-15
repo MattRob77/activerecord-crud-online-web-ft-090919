fd443cca46c2cf05e129c9e7b73f592fdabbb966
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-15 18:27:03 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -8,7 +8,7 @@
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -35,12 +35,16 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   end
 end
 
+def can_get_the_first_item_in_the_database
+  Movie.first
+end
+
 def can_get_the_last_item_in_the_database
   Movie.last
 end
 
 def can_get_size_of_the_database
-  Movie.first 
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
@@ -52,11 +56,11 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find_by(title: "Title", release_date: "2000", director: "Me")
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
    Movie.where("release_date > 2002").order(release_date: :desc)
 end
